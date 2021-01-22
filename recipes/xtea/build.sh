5e2c71236c613b31179b20b4695e2d1249fe21d5
SimonCChu <chong.simon.chu@gmail.com>
2021-01-22 14:55:36 +0800

    update recipes

--- recipes/xtea/build.sh
+++ recipes/xtea/build.sh
@@ -1,6 +1,5 @@
 mkdir -p $PREFIX/bin
 cp bin/xtea $PREFIX/bin
 cp bin/xtea_hg19 $PREFIX/bin
-mkdir -p $PREFIX/lib/xtea
-cp xtea/*.{py} $PREFIX/lib/xtea/
-cp -r xtea/genotyping $PREFIX/lib/xtea/
+cp bin/xtea_long $PREFIX/bin
+cp -r xtea $PREFIX/lib/
