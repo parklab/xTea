9694290383377e32c792f2d37dc006fd8be6cb74
corinsexton <corinsexton@gmail.com>
2024-05-09 13:28:42 -0700

    fix rep_lib_annot paths

--- bin/run_xtea.py
+++ bin/run_xtea.py
@@ -66,10 +66,76 @@ def parse_toml_args():
     return p.parse_args()
 
 def setup_annotation_paths(rep,rep_lib_annot_dir,genome_reference,genome):
+
+
+        # elif s_rep_type==REP_TYPE_PSEUDOGENE:
+        #     sf_config_pseudogene = sf_config_prefix + "Pseudogene.config"
+        #     sf_anno = "ANNOTATION " + sf_folder_rep + "Pseudogene/hg38/gencode_v28_GRCh38_exon_annotation.out\n"
+        #     sf_anno1 = "ANNOTATION1 " + sf_folder_rep + "Pseudogene/hg38/gencode_v28_GRCh38_exon_annotation.out\n"
+        #     sf_copy_with_flank = "L1_COPY_WITH_FLANK " + sf_folder_rep + "Pseudogene/hg38/gencode_v28_GRCh38_transcript_masked.fa\n"
+        #     sf_flank = "SF_FLANK null\n"
+        #     sf_cns = "L1_CNS " + sf_folder_rep + "consensus/gencode_v28_GRCh38_transcript_masked.fa\n"#
+        #     write_to_config(sf_anno, sf_anno1, sf_ref, sf_gene_anno, sf_black_list, sf_copy_with_flank, sf_flank,
+        #                     sf_cns, sf_xtea, s_bl, s_bam1, s_bc_bam, s_tmp, s_tmp_clip, s_tmp_cns, sf_config_pseudogene)
+
+        # elif s_rep_type=="Mitochondrion":####for Mitochondrion
+        #     sf_config_Mitochondrion = sf_config_prefix + "Mitochondrion.config"
+        #     sf_anno = "ANNOTATION " + sf_folder_rep + "Mitochondrion/hg38/hg38_numtS.out\n"
+        #     sf_anno1 = "ANNOTATION1 " + sf_folder_rep + "Mitochondrion/hg38/hg38_numtS.out\n"
+        #     sf_copy_with_flank = "L1_COPY_WITH_FLANK " + sf_folder_rep +\
+        #                          "Mitochondrion/hg38/hg38_mitochondrion_copies_with_flank.fa\n"
+        #     sf_flank = "SF_FLANK null\n"
+        #     sf_cns = "L1_CNS " + sf_folder_rep + "consensus/mitochondrion.fa\n"
+        #     write_to_config(sf_anno, sf_anno1, sf_ref, sf_gene_anno, sf_black_list, sf_copy_with_flank, sf_flank,
+        #                     sf_cns, sf_xtea, s_bl, s_bam1, s_bc_bam, s_tmp, s_tmp_clip, s_tmp_cns, sf_config_Mitochondrion)
+
+
     annotation_paths = dict()
-    annotation_paths["sf_rep"] = f"{rep_lib_annot_dir}/{rep}/{genome}/{genome}_AluJabc_copies_with_flank.fa" #options.reference TODO!!!!
     annotation_paths["sf_rep_cns"] = f"{rep_lib_annot_dir}/consensus/{rep}.fa" #options.cns
     annotation_paths["sf_annotation"] = f"{rep_lib_annot_dir}/{rep}/{genome}/{genome}_{rep}.out" #options.annotation
+
+    if rep == 'ALU':
+        r = 'Alu'
+        annotation_paths["sf_rep"] = f"{rep_lib_annot_dir}/{r}/{genome}/{genome}_AluJabc_copies_with_flank.fa"
+        annotation_paths["sf_rep_cns"] = f"{rep_lib_annot_dir}/consensus/{rep}.fa" #options.cns
+        annotation_paths["sf_annotation"] = f"{rep_lib_annot_dir}/{r}/{genome}/{genome}_{r}.out" #options.annotation
+        # sf_anno1 = "ANNOTATION1 " + sf_folder_rep + "Alu/hg38/hg38_Alu.out\n"
+        # sf_flank = "SF_FLANK null\n"
+    elif rep == 'L1':
+        annotation_paths["sf_rep"] = f"{rep_lib_annot_dir}/LINE/{genome}/{genome}_L1HS_copies_larger_5K_with_flank.fa"
+        annotation_paths["sf_rep_cns"] = f"{rep_lib_annot_dir}/consensus/LINE1.fa" #options.cns
+        annotation_paths["sf_annotation"] = f"{rep_lib_annot_dir}/LINE/{genome}/{genome}_L1_larger_500_with_all_L1HS.out" #options.annotation
+        # sf_anno1 = "ANNOTATION1 " + sf_folder_rep + "LINE/hg38/hg38_L1.fa.out\n"
+        # sf_flank = "SF_FLANK " + sf_folder_rep + "LINE/hg38/hg38_FL_L1_flanks_3k.fa\n"
+    elif rep == 'SVA':
+        annotation_paths["sf_rep"] = f"{rep_lib_annot_dir}/{rep}/{genome}/{genome}_SVA_copies_with_flank.fa"
+        annotation_paths["sf_rep_cns"] = f"{rep_lib_annot_dir}/consensus/{rep}.fa" #options.cns
+        annotation_paths["sf_annotation"] = f"{rep_lib_annot_dir}/{rep}/{genome}/{genome}_{rep}.out" #options.annotation
+        #     sf_anno1 = "ANNOTATION1 " + sf_folder_rep + "SVA/hg38/hg38_SVA.out\n"
+        #     sf_flank = "SF_FLANK " + sf_folder_rep + "SVA/hg38/hg38_FL_SVA_flanks_3k.fa\n"
+    elif rep == 'HERV':
+        annotation_paths["sf_rep"] = f"{rep_lib_annot_dir}/{rep}/{genome}/{genome}_HERV_copies_with_flank.fa"
+        annotation_paths["sf_rep_cns"] = f"{rep_lib_annot_dir}/consensus/{rep}.fa" #options.cns
+        annotation_paths["sf_annotation"] = f"{rep_lib_annot_dir}/{rep}/{genome}/{genome}_{rep}.out" #options.annotation
+        #     sf_anno1 = "ANNOTATION1 " + sf_folder_rep + "HERV/hg38/hg38_HERV.out\n"
+        #     sf_flank = "SF_FLANK null\n"
+    # elif rep == "MSTA":  ## TODO NOT TESTED
+    #     annotation_paths["sf_rep"] = f"{rep_lib_annot_dir}/{rep}/{genome}/{genome}_MSTA_copies_with_flank.fa"
+    #     annotation_paths["sf_rep_cns"] = f"{rep_lib_annot_dir}/consensus/{rep}.fa" #options.cns
+    #     annotation_paths["sf_annotation"] = f"{rep_lib_annot_dir}/{rep}/{genome}/{genome}_{rep}.out" #options.annotation
+    #     #     sf_anno1 = "ANNOTATION1 " + sf_folder_rep + "MSTA/hg38/hg38_MSTA.out\n"
+    #     #     sf_flank = "SF_FLANK null\n"
+    # elif rep == "Pseudogene":
+    #     annotation_paths["sf_rep"] = f"{rep_lib_annot_dir}/{rep}/{genome}/gencode_v28_GRCh38_transcript_masked.fa"
+    #     annotation_paths["sf_rep_cns"] = f"{rep_lib_annot_dir}/consensus/gencode_v28_GRCh38_transcript_masked.fa" #options.cns
+    #     annotation_paths["sf_annotation"] = f"{rep_lib_annot_dir}/Pseudogene/{genome}/gencode_v28_GRCh38_exon_annotation.out" #options.annotation
+    #     #     sf_anno1 = "ANNOTATION1 " + sf_folder_rep + "Pseudogene/hg38/gencode_v28_GRCh38_exon_annotation.out\n"
+    #     #     sf_flank = "SF_FLANK null\n"
+    # elif rep == "Mitochondrion":
+    #     annotation_paths["sf_rep"] = f"{rep_lib_annot_dir}/Mitochondrion/hg38/hg38_mitochondrion_copies_with_flank.fa"
+    #     annotation_paths["sf_rep_cns"] = f"{rep_lib_annot_dir}/consensus//mitochondrion.fa" #options.cns
+    #     annotation_paths["sf_annotation"] = f"{rep_lib_annot_dir}/Mitochondrion/hg38/hg38_numtS.out" #options.annotation
+        
     annotation_paths["sf_ref"] = genome_reference
 
     return annotation_paths
